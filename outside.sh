2fcac061a4f58d8d459c86844f5f22e5400290ed
Rik Goldman <rikgoldman@gmail.com>
2012-12-13 14:34:05 -0500

    Update outside.sh

--- outside.sh
+++ outside.sh
@@ -4,7 +4,8 @@
 #apt-get install weather
 #weather kdmh|grep Temperature
 #weather kdmh|grep Temperature|cut -b 25,26,27,28
-baltimore=$(weather kdmh|grep Temperature|cut -b 25,26,27,28)
+#baltimore=$(weather kdmh|grep Temperature|cut -b 25,26,27,28)
 #or
-dc=$curl http://weather.noaa.gov/pub/data/observations/metar/decoded/KDCA.TXT|grep Temperature|cut -b 22,23,24,25)
+#dc=$curl http://weather.noaa.gov/pub/data/observations/metar/decoded/KDCA.TXT|grep Temperature|cut -b 22,23,24,25)
+dc=$(curl -s http://weather.noaa.gov/pub/data/observations/metar/decoded/KDCA.TXT|grep Temperature|cut -c 20-)
 echo dc
