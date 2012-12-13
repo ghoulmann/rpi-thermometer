06bddce7179c4385ede86ecaa1bf124f28425759
Rik Goldman <rikgoldman@gmail.com>
2012-12-13 15:06:39 -0500

    Update outside.sh

--- outside.sh
+++ outside.sh
@@ -7,5 +7,6 @@
 #baltimore=$(weather kdmh|grep Temperature|cut -b 25,26,27,28)
 #or
 #dc=$curl http://weather.noaa.gov/pub/data/observations/metar/decoded/KDCA.TXT|grep Temperature|cut -b 22,23,24,25)
-dc=$(curl -s http://weather.noaa.gov/pub/data/observations/metar/decoded/KDCA.TXT|grep Temperature|cut -c 20-)
+#dc=$(curl -s http://weather.noaa.gov/pub/data/observations/metar/decoded/KDCA.TXT|grep Temperature|cut -c 20-)
+#dc=$(curl -s http://weather.noaa.gov/pub/data/observations/metar/decoded/KDCA.TXT|grep Temperature|cut -c 13-|cut -d " " -f 4)
 echo dc
