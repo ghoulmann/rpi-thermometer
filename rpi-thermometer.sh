425a2c41c39f6e07dd8a9199a45c60100c491194
Rik Goldman <rikgoldman@gmail.com>
2013-02-05 13:33:06 -0500

    dual wields

--- rpi-thermometer.sh
+++ rpi-thermometer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Copyright 2012 Rik Goldman, Chelsea School Students (T.A., L. P. D., J. A. B.)
+#Copyright 2012 by Rik Goldman, Chelsea School Students (T.A., L. P. D., J. A. B.)
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -21,7 +21,7 @@
 executeable="rpi-thermometer"
 webroot="/usr/share/nginx/www" #where to put the web files (index.html)
 graphdir="/usr/share/nginx/www/images" #where to put the graphics (by default, for web)
-sensor1="/mnt/1wire/10.98C57C020800/temperature" #sensor1. Read like a file.
+sensor="/mnt/1wire/10.98C57C020800/temperature" #sensor. Read like a file.
 thermometer_log_path="/var/log/temperature" #where to put temp log
 thermometer_log="temperature.log" #by default, /var/log/temperature/temperature.log (thinking logrotate)
 path_to_db="/var/local" #not sure where by FHS guidelines
@@ -31,11 +31,10 @@ executable_dir="/usr/local/bin" #for rpi-thermometer.sh. Not sure by FHS convent
 origin=$(pwd) #unnecessary now
 stamp=$(date)
 key=$(date +%s) #epoch in linux; perhaps key field if we use sqlite
-weathers_station="KDCA"
-local_outside=$(curl -s http://weather.noaa.gov/pub/data/observations/metar/decoded/KDCA.TXT|grep Temperature|cut -c 13-|cut -d " " -f 4|cut -c 2-) #substitute $weather_station for KDCA
+weather_station="KDCA"
+local_outside=$(curl -s http://weather.noaa.gov/pub/data/observations/metar/decoded/$weather_station.TXT|grep Temperature|cut -c 13-|cut -d " " -f 4|cut -c 2-) #substitute $weather_station for KDCA
 dummy="" #dummy temperature for troubleshooting flow on a machine w/o sensors
-
-
+#To install prerequisites and create the database and other stuff required before first use, use with config argument: rpi-thermometer.sh config
 
 ###########
 #Functions#
@@ -99,7 +98,13 @@ create_database ()
 	RRA:AVERAGE:0.5:1:288 \
 	RRA:AVERAGE:0.5:12:168 \
 	RRA:AVERAGE:0.5:12:720 \
-	RRA:AVERAGE:0.5:288:365
+	RRA:AVERAGE:0.5:288:365 \
+	DS:outside:GAUGE:600:0:100 \
+        RRA:AVERAGE:0.5:1:12 \
+        RRA:AVERAGE:0.5:1:288 \
+        RRA:AVERAGE:0.5:12:168 \
+        RRA:AVERAGE:0.5:12:720 \
+        RRA:AVERAGE:0.5:288:365
 	check
 }
 
@@ -107,25 +112,25 @@ create_database ()
 log_temperature ()
 {
 	check_for_root
-	if [ $dummy == 0 ]; then
-		reading1=$(cat $sensor1) #test on machine with sensor
-		echo $reading1 #troubleshooting
-		reading1=`echo $reading1 | cut -c -4`
+	if [[ $dummy == "" ]]; then
+		celsius=$(cat $sensor) #test on machine with sensor
+		echo "Celsius: $celsius" #troubleshooting
+		celsius=`echo $celsius | cut -c -4`
 	else
-		reading1="$dummy"
+		celsius=$dummy
 	fi
-
-	fahrenheit=$(echo "scale=2;((9/5) * $reading1) + 32" |bc)
-	echo $fahrenheit #troubleshooting
+	fahrenheit=$(echo "scale=2;((9/5) * $celsius) + 32" |bc)
+	echo "fahrenheit: $fahrenheit" #troubleshooting
 	echo "Starting Log Process" #troubleshooting
 	echo "Preparing Date" #troubleshooting
-	line=$(echo "$stamp, $reading1 (c: sensor1), $fahrenheit (F: derived from censor 1)")
-	#write sensor1 information to log file
+	line=$(echo "$stamp, $celsius (Thermometer1 C), $fahrenheit (Thermometer1 F), $local_outside ($weather_station C)")
+	echo "This is the logged line: $line"
+	#write sensor information to log file
 	echo $line >> $thermometer_log_path/$thermometer_log
 
 	#write sensor data to rrdtool database
-	echo "writing sensor1 data to database"
-	rrdtool update $path_to_db/$db N:$reading1
+	echo "writing sensor data to database"
+	rrdtool update $path_to_db/$db N:$celsius:$local_outside
 
 	####################################
 	#write weather station web site#####
@@ -140,7 +145,7 @@ log_temperature ()
 		<body bgcolor="white" text="black">
 			<center><h1>Weather Station</h1></center>
 			<center><img src="images/temp_h.png"></center>
-			<p><center><bold>Last Update: $now |Temperature (Fahrenheit): $fahrenheit | Temperature (Celsius): $reading1</bold></center></p>
+			<p><center><bold>Last Update: $now |Temperature (Fahrenheit): $fahrenheit | Temperature (Celsius): $celsius</bold></center></p>
 EOD
 	echo "<pre>" >> $webroot/$weather_home
 	tail -100 $thermometer_log_path/$thermometer_log >> $webroot/$weather_home
@@ -152,12 +157,19 @@ graph_temperature ()
 {
 	echo "Creating graphs"
 	#Create temp_h.png - customized to see if it helps getting data graphed. Not sure it made a difference
-	rrdtool graph $graphdir/temp_h.png --start -1d --end now --x-grid MINUTE:10:HOUR:1:HOUR:2:0:%H:00 --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+	rrdtool graph $graphdir/temp_h.png \
+	--start -1d --end now \
+	--x-grid MINUTE:10:HOUR:1:HOUR:2:0:%H:00 \
+	--vertical-label "Celsius" \
+	--upper-limit 30 \
+	--lower-limit -5 \
+	DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Sensor [deg C]" \
+	DEF:outside=$path_to_db/$db:outside:AVERAGE LINE2:outside#FF0000:"Outside [deg C]"
 	#Create other graphs
-	rrdtool graph $graphdir/temp_d.png --start -1d --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
-	rrdtool graph $graphdir/temp_w.png --start -1w --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
-	rrdtool graph $graphdir/temp_m.png --start -1m --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
-	rrdtool graph $graphdir/temp_y.png --start -1y --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+	#rrdtool graph $graphdir/temp_d.png --start -1d --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+	#rrdtool graph $graphdir/temp_w.png --start -1w --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+	#rrdtool graph $graphdir/temp_m.png --start -1m --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+	#rrdtool graph $graphdir/temp_y.png --start -1y --vertical-label "Celsius" DEF:temp=$path_to_db/$db:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
 	#make graphs web accessible
 	chown -R www-data:www-data $graphdir/
 	
@@ -204,7 +216,11 @@ function configure()
 	check
 }
 
-
+#check usage
+#if [ $# -ne "2" ]; then
+#	usage#
+#	exit 1
+#fi
 
 #CONFIGURE
 if [ "$1" == "config" ]; then
@@ -222,12 +238,10 @@ elif [ "$1" == "graph" ]; then
 	graph_temperature
 	exit 0
 elif [ "$1" == "values" ]; then
-	echo "echo values here"     # TO DO
+	echo "values"
 	exit 0
 elif [ "$1" == "remove" ]; then
 	remove
-	exit 0
 else
 	usage
-	exit 0
 fi
